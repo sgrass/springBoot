b85ea5bd76fca0e5d1d5c555bcbd6d9c62d7bd85
xgrass <xgrass@foxmail.com>
2017-07-22 14:49:27 +0800

    tomcat依赖调整

--- src/main/java/com/cx/Application.java
+++ src/main/java/com/cx/Application.java
@@ -8,21 +8,22 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.cx.*.mapper")
-public class Application extends SpringBootServletInitializer{
+public class Application extends SpringBootServletInitializer {
 
-	/**
-	 * 打包为war的方式
-	 * 继承SpringBootServletInitializer类，重载configure方法
-	 * pom文件package修改为war
-	 * 增加依赖 <artifactId>spring-boot-starter-tomcat</artifactId>
-	 */
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-	return application.sources(Application.class);
-	}
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    /**
+     * 打包为war的方式
+     * 继承SpringBootServletInitializer类，重载configure方法
+     * pom文件package修改为war
+     * 增加依赖 <artifactId>spring-boot-starter-tomcat</artifactId>
+     * TODO idea运行需把tomcat依赖scope修改为compile
+     */
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/com/cx/web/HelloController.java
+++ src/main/java/com/cx/web/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -35,4 +36,16 @@ public class HelloController {
 		System.out.println(123);
 		return "aa.html";
 	}
+	
+	@RequestMapping(value = "/log")
+  @ResponseBody
+  public String log(){
+      //日志级别从低到高分为TRACE < DEBUG < INFO < WARN < ERROR < FATAL，如果设置为WARN，则低于WARN的信息都不会输出。
+      log.trace("日志输出 trace");
+      log.debug("日志输出 debug");
+      log.info("日志输出 info");
+      log.warn("日志输出 warn");
+      log.error("日志输出 error");
+      return "aaa";
+  }
 }
